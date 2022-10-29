0883221144c7c2100c71dc6092741d0a67022806
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-10-29 17:38:38 +0530

    Time: 162 ms (71.97%), Space: 37.1 MB (20.12%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
+    vector<vector<int>> mp;
     bool p(string & s, int l, int r){
-        while(l <= r) if(s[l++] != s[r--]) return false;
-        return true;
+        if(l >= r) return true;
+        if(mp[l][r] != -1) return mp[l][r];
+        if(s[l] == s[r]) return mp[l][r] = p(s,l+1,r-1);
+        return mp[l][r] =false;
     }
     int minCut(string s) {
         int n = s.size();
+        mp.resize(n, vector<int>(n,-1));
         vector<int> dp(n+1,0);
         dp[n] = -1;
         for(int ind = n-1; ind >= 0; ind--){
