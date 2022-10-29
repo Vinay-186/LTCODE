02d117305975173dd5263fa9fb70f574413a844f
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-10-29 17:43:19 +0530

    Time: 11 ms (98.66%), Space: 6.3 MB (99.31%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> mp;
-    bool p(string & s, int l, int r){
-        if(l >= r) return true;
-        if(mp[l][r] != -1) return mp[l][r];
-        if(s[l] == s[r]) return mp[l][r] = p(s,l+1,r-1);
-        return mp[l][r] =false;
-    }
-    int minCut(string s) {
-        int n = s.size();
-        mp.resize(n, vector<int>(n,-1));
-        vector<int> dp(n+1,0);
-        dp[n] = -1;
-        for(int ind = n-1; ind >= 0; ind--){
-            int mini = INT_MAX;
-            for(int i = ind; i <n ;i++) if(p(s,ind,i)) mini = min(mini,1+dp[i+1]);
-            dp[ind] = mini;
-        }        
-        return dp[0];
+    int minCut(string a) {
+        int n=a.size();
+        int dp[n+2],i,j,k;
+        for(i=0;i<=n;i++) dp[i] = i-1;
+        for(i=0;i<n;i++){
+            for(j=0;i-j>=0&&i+j<n&&a[j+i]==a[i-j];j++) dp[i+j+1]=min(dp[i+j+1],dp[i-j]+1);
+            for(j=1;i-j+1>=0&&i+j<n&&a[j+i]==a[i-j+1];j++) dp[i+j+1]=min(dp[i+j+1],dp[i-j+1]+1);
+        }
+        return dp[n];
     }
 };
