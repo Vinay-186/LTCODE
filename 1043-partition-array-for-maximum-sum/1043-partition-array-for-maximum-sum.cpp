77cd67b4c7d22272c8dc3043ee1ffafe20fab292
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-10-29 18:48:13 +0530

    Time: 424 ms (6.30%), Space: 8.6 MB (35.02%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int k ;
-    int n ;
-    vector<int> dp;
-    int msap(vector<int>& arr, int ind){
-        if(ind == n) return 0;
-        if(dp[ind] != -1) return dp[ind];
-        int ans = INT_MIN;
-        for(int j = ind; j < ind + k && j < n; j++){
-            int maxi = *max_element(arr.begin() + ind,arr.begin() + j+1);
-            int cost = maxi * (j - ind + 1) + msap(arr,j+1);
-            ans = max(ans, cost);
-        }
-        return dp[ind] = ans;
-    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        this->k = k;
-        n = arr.size();
-        dp.resize(n,-1);
-        return msap(arr,0);
+        int n = arr.size();
+        vector<int> dp(n+1,0);
+        for(int ind = n-1; ind >= 0; ind--){
+            int ans = INT_MIN;
+            for(int j = ind; j < ind + k && j < n; j++){
+                int maxi = *max_element(arr.begin()+ind, arr.begin() + j + 1);
+                int cost = maxi * (j - ind + 1) + dp[j+1];
+                ans = max(ans, cost);
+            }
+            dp[ind] = ans;
+        }
+        return dp[0];
     }
 };
