3a28c3426e4248ccfe555b1dcba706e9d44ffe48
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-10-29 18:49:37 +0530

    Time: 269 ms (7.91%), Space: 8.6 MB (35.02%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -5,11 +5,8 @@ public:
         vector<int> dp(n+1,0);
         for(int ind = n-1; ind >= 0; ind--){
             int ans = INT_MIN;
-            for(int j = ind; j < ind + k && j < n; j++){
-                int maxi = *max_element(arr.begin()+ind, arr.begin() + j + 1);
-                int cost = maxi * (j - ind + 1) + dp[j+1];
-                ans = max(ans, cost);
-            }
+            for(int j = ind; j < ind + k && j < n; j++)
+                ans = max(ans, (*max_element(arr.begin()+ind, arr.begin() + j + 1)) * (j - ind + 1) + dp[j+1]);
             dp[ind] = ans;
         }
         return dp[0];
