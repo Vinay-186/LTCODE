25b5d3ec6145aa131b98c8e661620a7c0aec1c95
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-11-22 20:14:53 +0530

    Time: 170 ms (52.55%), Space: 48.4 MB (79.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,16 +2,8 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, 0);
-        dp[n-1] = dp[n] = 1;
-        for(int i = n-2; i >= 0; i--){
-            bool f = false;
-            for(int j = nums[i]; j >= 1; j--){
-                if(f == true) break;
-                f = (i + j >= n-1)? true : dp[i+j];
-            }
-            dp[i] = f;   
-        }
-        return dp[0];
+        int lastPos = n - 1;
+        for (int i = n - 1; i >= 0; i--) if (i + nums[i] >= lastPos) lastPos = i;
+        return lastPos == 0;
     }
 };
