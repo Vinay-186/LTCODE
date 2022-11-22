8cd5e61128db5f17be8cabd98dbff3b32050c9b4
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-11-22 18:00:51 +0530

    Time: 1079 ms (9.30%), Space: 153.5 MB (7.04%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int sz,n;
-    vector<vector<int>> dp;
-    int ns(int ind, int sum){
-        if(sum > n) return 1e8;
-        if(ind <= 0){
-            if(sum == n){
-                return 0;
-            }
-            return 1e8;
-        }
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        int include = 1 + ns(ind, sum + ind*ind);
-        int exclude = ns(ind-1, sum);
-        return dp[ind][sum] = min(include, exclude);
-    }
     int numSquares(int n) {
-       sz = pow(n, 0.5);
-       this->n = n;
-       dp.resize(sz+1, vector<int>(n+1,-1));
-       return ns(sz,0);
+       int sz = pow(n, 0.5);
+       vector<vector<int>> dp(sz+1, vector<int>(n+1,1e8));
+       dp[0][n] = 0;
+       for(int i = 1; i <= sz; i++){
+           for(int j = n; j >= 0; j--){
+               int include = 1e8;
+               if(j + i*i <= n)include = 1 + dp[i][j+i*i];
+               int exclude = dp[i-1][j];
+               dp[i][j] = min(include, exclude);
+           }
+       }
+       return dp[sz][0];
     }
 };
