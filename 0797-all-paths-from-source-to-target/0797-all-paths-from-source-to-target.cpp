8a1d5d5a2741248377b7c6d734f81e8f3945c0d6
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2022-12-31 01:12:27 +0530

    Time: 10 ms (95.48%), Space: 11.9 MB (59.43%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    vector<int> seen;
     int n;
     void helper(vector<vector<int>>& g,int ind, vector<int>& ans){
         if(ind == n-1){
@@ -9,21 +8,15 @@ public:
             return;
         }
         for(int i : g[ind]){
-            if(seen[i] != 1){
                 ans.push_back(i);
-                seen[i] = 1;
                 helper(g,i,ans);
-                seen[i] = 0;
                 ans.pop_back();
-            }
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<int> ans;
         n = graph.size();
-        seen.resize(n,0);
         ans.push_back(0);
-        seen[0] = 1;
         helper(graph, 0, ans);
         return res;
     }
