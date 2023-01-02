0510b86b5d34d170e9bb39ef2d29a278e7a318a9
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-02 18:41:05 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (24.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int n = word.size();
-        if(n == 1) return true;
-        set<char> caps, lows;
-        for(char c = 'A'; c <= 'Z'; c++) caps.insert(c);
-        for(char c = 'a'; c <= 'z'; c++) lows.insert(c);
-        string temp;
-        if(lows.find(word[0]) != lows.end()){
-            temp = word.substr(1);
-            for(char& c : temp){
-                if(caps.find(c) != caps.end()) return false;
-            }
-        }else if(caps.find(word[1]) != caps.end()){
-            if(n > 2) temp = word.substr(2);
-            else temp = "";
-            for(char& c : temp){
-                if(lows.find(c) != lows.end()) return false;
-            }
-        }else{
-            if(n > 2) temp = word.substr(2);
-            else temp = "";
-            for(char& c : temp){
-                if(caps.find(c) != caps.end()) return false;
+        int cnt=0;
+        int loc;
+        for(int i=0;i<word.length();i++){
+            if(word[i]>='A' && word[i]<='Z'){
+                cnt++;
+                loc = i;
             }
         }
-        return true;
+        if(cnt == word.length() || cnt == 0 || (cnt==1 && loc==0)) return true;
+        return false;
     }
 };
