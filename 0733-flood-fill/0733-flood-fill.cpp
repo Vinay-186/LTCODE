3ce2676f1e8e1e5a82dcb3d889a33523f36bbedb
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-07 00:37:19 +0530

    Time: 14 ms (60.35%), Space: 14.5 MB (12.28%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int m, n, color, temp;
-    vector<vector<int>> seen;
-    void helper(vector<vector<int>>& img, int i, int j){
-        if(i < 0 || j < 0 || i >= n || j >= m || seen[i][j] == 1) return;
-        img[i][j] = color;
-        seen[i][j] = 1;
-        if(i-1 >= 0 && img[i-1][j] == temp) helper(img, i-1,j);
-        if(i+1 < n && img[i+1][j] == temp) helper(img, i+1,j);
-        if(j-1 >= 0 && img[i][j-1]== temp) helper(img, i, j-1);
-        if(j+1 < m && img[i][j+1] == temp) helper(img, i, j+1);
-        
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        this->color = color;
-        n = image.size();
-        m = image[0].size();
-        seen.resize(n,vector<int>(m,0));
-        temp = image[sr][sc];
-        helper(image, sr,sc);
+        int n = image.size();
+        int m = image[0].size();
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int temp = image[sr][sc];
+        vector<vector<int>> seen(n, vector<int>(m,0));
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            image[i][j] = color;
+            seen[i][j] = 1;
+            if(i-1 >= 0 && seen[i-1][j] != 1 && image[i-1][j] == temp) q.push({i-1,j});
+            if(i+1 < n && seen[i+1][j] != 1 && image[i+1][j] == temp) q.push({i+1,j});
+            if(j-1 >= 0 && seen[i][j-1] != 1 && image[i][j-1] == temp) q.push({i,j-1});
+            if(j+1 < m && seen[i][j+1] != 1 && image[i][j+1] == temp) q.push({i,j+1});
+        }
         return image;
     }
 };
