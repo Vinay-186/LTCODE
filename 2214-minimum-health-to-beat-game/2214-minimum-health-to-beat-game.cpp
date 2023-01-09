e27166bc66c8a796817bda2eb6688ce63ee2b0b4
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-09 19:43:16 +0530

    Time: 101 ms (91.38%), Space: 59.1 MB (65.86%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cpp
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     long long minimumHealth(vector<int>& damage, int armor) {
-       vector<int>::iterator maxi = max_element(damage.begin(), damage.end());
-       if(*maxi > armor) *maxi -= armor;
-       else *maxi = 0;
-       long long ans = 1;
-       int n = damage.size();
-       for(int i = 0 ;i < n; i++) ans += damage[i];
-       return ans;
+       return accumulate(damage.begin(), damage.end(), 1ll) - min(*max_element(damage.begin(), damage.end()), armor);
     }
 };
