1faa95b23740e478752b71b5bcf9230ad21db4be
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-22 19:54:59 +0530

    Time: 435 ms (52.94%), Space: 197.9 MB (19.33%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-	vector<vector<int>> dp;
-	bool p(string& s, int i, int j) {
-		if (i >= j) return true;
-		if (dp[i][j] != -1) return dp[i][j];
-		if (s[i] == s[j]) return dp[i][j] = p(s, i + 1, j - 1);
-		return dp[i][j] = false;
-	}
 	bool checkPartitioning(string s) {
 		int n = s.size();
-		dp.resize(n,vector<int> (n,-1));
-		for(int i=0;i<n;i++)for(int j=i+1;j<n-1;j++)if(p(s,0,i) && p(s,i+1,j) && p(s,j+1,n-1)) return true;
+		vector<vector<int>> p(n+1,vector<int> (n,1));
+        for(int i = n-1 ; i >= 0; i--)for(int j = i+1; j < n; j++)p[i][j] = (s[i] == s[j])? p[i+1][j-1] : 0;
+		for(int i=0;i<n;i++)for(int j=i+1;j<n-1;j++)if(p[0][i] && p[i+1][j] && p[j+1][n-1]) return true;
 		return false;
 	}
 };
