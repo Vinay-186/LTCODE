017bc7565088fe267e1ccf2ed720553120541e8b
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-22 14:18:10 +0530

    Time: 168 ms (51.25%), Space: 75.8 MB (31.59%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<vector<string>> res;
     int n;
-    bool p(string& temp){
-        int i = 0,j = temp.size()-1;
-        while(i < j) if(temp[i++] != temp[j--]) return false;
-        return true;
+    vector<vector<int>> dp;
+    bool p(string& s, int i, int j){
+        if(i > j) return true;
+        if(s[i] != s[j]) return false;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = p(s,i+1,j-1);
     }
     void helper(string& s,int ind,vector<string>& ans){
         if(ind == n){
             res.push_back(ans);
             return ;    
         }
-        string temp;
         for(int i = ind; i < n; i++){
-            temp += s[i];
-            if(p(temp)){
-                ans.push_back(temp);
+            if(p(s,ind, i)){
+                ans.push_back(s.substr(ind, i-ind+1));
                 helper(s,i+1,ans);
                 ans.pop_back();
             }
@@ -24,6 +24,7 @@ public:
     }
     vector<vector<string>> partition(string s) {
         n = s.size();
+        dp.resize(n, vector<int>(n,-1));
         vector<string> ans;
         helper(s,0,ans);
         return res;
