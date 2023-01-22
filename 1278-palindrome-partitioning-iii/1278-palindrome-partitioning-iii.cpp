bca9342f2a904bdd97148c89d57eeb3a12e1b796
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-22 18:59:50 +0530

    Time: 40 ms (35.74%), Space: 6.7 MB (65.86%) - LeetHub

---
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n; 
+    vector<vector<int>> dp, d;
+    int p(string& s, int i, int j){
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        return  dp[i][j] = (s[i] == s[j])? p(s, i+1, j-1) : 1 + p(s, i+1, j-1);
+    }
+    int helper(string& s, int ind, int k){
+        if(k < 0) return 1e8;
+        if(ind == n){
+            if(k == 0) return 0;
+            return 1e8;
+        }
+        if(d[ind][k] != -1) return d[ind][k];
+        int mini = INT_MAX;
+        for(int i = ind; i < n;i++){
+            int cnt = p(s,ind, i) + helper(s,i+1,k-1);
+            mini = min(mini, cnt);
+        }
+        return d[ind][k] = mini;
+    }
+    int palindromePartition(string s, int k) {
+        n = s.size();
+        if(n == k) return 0;
+        d.resize(n,vector<int>(k+1, -1));
+        dp.resize(n, vector<int>(n,-1));
+        return helper(s,0,k);
+    }
+};
