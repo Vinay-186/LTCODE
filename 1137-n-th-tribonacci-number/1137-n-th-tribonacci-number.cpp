fa503cc42e97d228b3bbd8d801307b63898baec9
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-30 19:10:56 +0530

    Time: 3 ms (38.83%), Space: 6 MB (73.21%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     int tribonacci(int n) {
         if(n <= 2) return (n <= 1)? n : 1;
-        vector<int> dp(n+1);
-        dp[0] = 0, dp[1] = dp[2] = 1;
-        for(int i = 3; i <= n; i++) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        return dp[n];
+        int t0 = 0,t1 = 1,t2 = 1;
+        int ans;
+        for(int i = 3; i <= n; i++){
+            ans = t0 + t1 + t2;
+            t0 = t1, t1 = t2, t2 = ans;
+        }
+        return ans;
     }
 };
