ca438a3796f62cb91a89e72f1ac9bb3e87fa7b89
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-31 18:58:47 +0530

    Time: 1099 ms (5.19%), Space: 135.3 MB (11.04%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> dp;
-    int bts(vector<vector<int>>& as, int ind, int prev){
-        if(ind == n) return 0;
-        if(dp[ind][prev] != -1) return dp[ind][prev];
-        int pick, notpick;
-        pick = notpick = INT_MIN;
-        if(prev == 0 || as[ind][0] == as[prev-1][0] || as[ind][1] >= as[prev-1][1]){
-            pick = as[ind][1] + bts(as,ind+1, ind+1);
-        }
-        notpick = bts(as,ind+1,prev);
-        return dp[ind][prev] = max(pick,notpick);
-    }
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        n = ages.size();
-        dp.resize(n,vector<int>(n,-1));
+        int n = ages.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         vector<vector<int>> as(n, vector<int>(2,0));
         for(int i = 0; i < n; i++) as[i][0] = ages[i], as[i][1] = scores[i];
         sort(as.begin(), as.end());
-        return bts(as,0,0);
+        for(int i = n-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                int p,np;
+                p = np = INT_MIN;
+                if(j == 0 || as[i][0] == as[j-1][0] || as[i][1] >= as[j-1][1]) p = as[i][1] + dp[i+1][i+1];
+                np = dp[i+1][j];
+                dp[i][j] = max(p,np);
+            }
+        }
+        return dp[0][0];
     }
 };
