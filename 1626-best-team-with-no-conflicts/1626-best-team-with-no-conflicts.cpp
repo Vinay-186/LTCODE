d95ee76ff8ff4cc06eccb82184fa2324ce3782c0
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-01-31 19:10:08 +0530

    Time: 383 ms (33.44%), Space: 21.7 MB (45.13%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n = ages.size();
-        vector<int> cur(n+1,0), prev(n+1,0);
         vector<vector<int>> as(n, vector<int>(2,0));
         for(int i = 0; i < n; i++) as[i][0] = ages[i], as[i][1] = scores[i];
         sort(as.begin(), as.end());
-        for(int i = n-1; i >= 0; i--){
-            for(int j = n-1; j >= 0; j--){
-                int p,np;
-                p = np = INT_MIN;
-                if(j == 0 || as[i][0] == as[j-1][0] || as[i][1] >= as[j-1][1]) p = as[i][1] + prev[i+1];
-                np = prev[j];
-                cur[j] = max(p,np);
+        vector<int> dp(n);
+        for(int i = 0; i < n; i++) dp[i] = as[i][1];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(as[i][0] == as[j][0] || as[i][1] >= as[j][1]){
+                    dp[i] = max(dp[i], as[i][1] + dp[j]);
+                }
             }
-            prev = cur;
         }
-        return prev[0];
+        return *max_element(dp.begin(), dp.end());
     }
 };
