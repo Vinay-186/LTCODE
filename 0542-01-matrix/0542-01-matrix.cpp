a805272e069cf05bbdf90261a53b951eaf1d7dc4
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-04-01 18:49:05 +0530

    Time: 85 ms (70.29%), Space: 33.2 MB (47.00%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<vector<int>> dist(rows, vector<int> (cols, 1e8));
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    dist[i][j] = 0;
-                } else {
-                    if (i > 0)
-                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
-                    if (j > 0)
-                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> vis(m, vector<int>(n,0)), ans(m, vector<int>(n, 0));
+        queue<pair<int,int>> q;
+        for(int i = 0; i < m ;i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0){
+                    q.push({i,j});
+                    vis[i][j] = 1;
+                    ans[i][j] = 0;
                 }
             }
         }
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 0; j--) {
-                if (i < rows - 1)
-                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
-                if (j < cols - 1)
-                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
+        int step = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i = 0; i < sz; i++){
+                auto [x,y] = q.front();
+                q.pop();
+                ans[x][y] = step;
+                if(x -1 >= 0 && !vis[x-1][y]) q.push({x-1,y}), vis[x-1][y]++;
+                if(x +1 < m && !vis[x+1][y]) q.push({x+1,y}), vis[x+1][y]++;
+                if(y -1 >= 0 && !vis[x][y-1]) q.push({x,y-1}), vis[x][y-1]++;
+                if(y +1 < n && !vis[x][y+1]) q.push({x,y+1}), vis[x][y+1]++;
             }
+            step++;
         }
-        return dist;
+        return ans;
     }
 };
