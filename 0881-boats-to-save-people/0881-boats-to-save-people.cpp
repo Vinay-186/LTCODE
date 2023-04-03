0c101fd697022252fcc077ea05124bbbdd9f9dd5
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-04-03 16:40:50 +0530

    Time: 82 ms (83.76%), Space: 43.7 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -8,18 +8,18 @@ public:
         int l = 0, r = n -1 ;
         int count = 0;
         vector<int> vis(n, 0);
-        while(l < r){
+        while(l <= r){
             if(people[l] + people[r] <= limit){
-                count++;
-                vis[l] = vis[r] = 1;
+                // vis[l] = vis[r] = 1;
                 l++, r--;
             }else{
                 r--;
             }
+            count++;
         }
-        for(int i = 0; i < n; i++){
-            if(!vis[i]) count++;
-        }
+        // for(int i = 0; i < n; i++){
+        //     if(!vis[i]) count++;
+        // }
         return count;
     }
 };    
