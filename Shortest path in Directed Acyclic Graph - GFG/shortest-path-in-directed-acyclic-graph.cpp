8310ded439e114074585976edab2edfed522429e
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-04-04 20:50:01 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -7,6 +7,14 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
+  private : 
+    void topoSort(int ind, vector<vector<int>>& adj, vector<int>& vis, stack<int>& topo){
+        vis[ind] = 1;
+        for(int& j : adj[ind]){
+            if(!vis[j])topoSort(j, adj, vis, topo);
+        }
+        topo.push(ind);
+    }
   public:
      vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
         // code here
@@ -17,18 +25,17 @@ class Solution {
             mp[{edges[i][0], edges[i][1]}] = edges[i][2];
         }
         vector<int> dist(N, 1e8);
+        vector<int> vis(N, 0);
+        stack<int> topo;
+        for(int i = 0; i < N; i++){
+            if(!vis[i]) topoSort(i, adj, vis, topo);
+        }
         dist[0] = 0;
-        queue<int> q;
-        q.push(0);
-        while(!q.empty()){
-            int sz = q.size();
-            for(int i = 0; i < sz; i++){
-                int node = q.front();
-                q.pop();
-                for(int& j : adj[node]){
-                    q.push(j);
-                    dist[j] = min(dist[j],dist[node] +  mp[{node, j}]);
-                }
+        while(!topo.empty()){
+            int node = topo.top();
+            topo.pop();
+            for(int& i : adj[node]){
+                dist[i] = min(dist[i], dist[node] + mp[{node, i}]);
             }
         }
         for(int i = 0; i < N; i++){
