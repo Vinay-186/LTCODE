a0f5cbb39c520f2ff53be9b1c7ab5dc4b7a6c852
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-04-08 01:24:58 +0530

    Time: 7 ms (61.34%), Space: 8.6 MB (87.83%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,25 +21,27 @@ public:
 
 class Solution {
 public:
-    void dfs(Node* node, Node* clone, map<Node*, Node*>& vis){
-        vis[node] = clone;
-        for(Node* i : node->neighbors){
-            if(vis.find(i) == vis.end()){
-                Node* temp = new Node(i->val);
-                clone->neighbors.push_back(temp);
-                dfs(i, temp, vis);
-            }
-            else {
-                clone->neighbors.push_back(vis[i]);
-            }
-        }
-    }
     Node* cloneGraph(Node* node) {
         if(!node) return nullptr;
         Node* root = new Node(node->val);
         map<Node*, Node*> vis;
         vis[node] = root;
-        dfs(node, root, vis);
+        queue<pair<Node*, Node*>> q;
+        q.push({node, root});
+        while(!q.empty()){
+            auto [n, c] = q.front();
+            q.pop();
+            for(Node* i : n->neighbors){
+                if(vis.find(i) == vis.end()){
+                    Node* temp = new Node(i->val);
+                    vis[i] = temp;
+                    c->neighbors.push_back(temp);
+                    q.push({i, temp});
+                }else{
+                    c->neighbors.push_back(vis[i]);
+                }
+            }
+        }
         return root;
     }
 };
