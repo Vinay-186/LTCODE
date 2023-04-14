ed57048fe72f95453a0d9cc7d41c263e07e558d6
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-04-14 15:04:40 +0530

    Time: 112 ms (46.62%), Space: 53 MB (28.57%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -4,11 +4,10 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    if(obj == null) return false;
-    if(obj.constructor == classFunction) return true;
-    obj = Object.getPrototypeOf(obj);
-    return checkIfInstanceOf(obj, classFunction);
+  if (obj === null || obj == undefined || classFunction == undefined || classFunction == null) return false;
+  return obj.constructor === classFunction || checkIfInstanceOf(Object.getPrototypeOf(obj), classFunction);
 };
+
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
