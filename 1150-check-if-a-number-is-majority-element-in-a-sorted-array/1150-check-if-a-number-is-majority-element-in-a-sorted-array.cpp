415b960a34ad0ca7e38c1eb5cacb34512a2ff0bd
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-06-14 12:50:17 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (41.17%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -1,17 +1,35 @@
 class Solution {
+private : 
+    int lower_bound(vector<int>& nums, int target){
+        int l = 0, r = nums.size()-1;
+        int index = nums.size();
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] >= target){
+                r = mid-1;
+                index = mid;
+            }else{
+                l = mid+1;
+            }
+        }
+        return index;
+    }
+    
+    int upper_bound(vector<int>& nums, int target){
+        int l = 0, r = nums.size() -1;
+        int index = nums.size();
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] <= target) l = mid+1;
+            else r = mid-1, index = mid;
+        }
+        return index;
+    }
 public:
     bool isMajorityElement(vector<int>& nums, int target) {
-        int count = 0;
-        int candidate = -1;// Candidate for Boyer Moore Algo;
-        int freq = 0;  // freq of target element in the nums;
-        for(int num : nums){
-            if(count == 0){
-                candidate = num;
-            }
-            count += (num == candidate)? 1 : -1;
-            if(num == target) freq++;
-        }
-        if(target == candidate && freq > nums.size()/2) return true;
-        return false;
+        int first = lower_bound(nums, target);
+        int last = upper_bound(nums, target);
+        int count = last - first;
+        return count > nums.size()/2;
     }
 };
