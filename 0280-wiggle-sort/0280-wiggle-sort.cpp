b4ed3f6271a44efa83b763ad8b94717a0fa0c1df
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-06-16 17:25:36 +0530

    Time: 13 ms (48.46%), Space: 13.5 MB (45.56%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.cpp
+++ 0280-wiggle-sort/0280-wiggle-sort.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int> temp = nums;
-        sort(temp.begin(), temp.end());
         int n = nums.size();
-        int i = 0, j = n-1;
-        int t = 0;
-        while(t < n){
-            if(i == j){
-                nums[t++] = temp[i];
-            }else{
-                nums[t++] = temp[i];
-                nums[t++] = temp[j];
-                i++, j--;
-            }
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i <= n-2; i+=2){
+            swap(nums[i], nums[i+1]);
         }
     }
 };
