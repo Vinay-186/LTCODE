4e22ab380e2f32493d3ce611d456d8eb79b15bc7
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-06-30 18:09:37 +0530

    Time: 83 ms (75.93%), Space: 8.8 MB (79.03%) - LeetHub

--- 1230-toss-strange-coins/1230-toss-strange-coins.cpp
+++ 1230-toss-strange-coins/1230-toss-strange-coins.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int n;
-    vector<vector<double>> dp;
     double probabilityOfHeads(vector<double>& prob, int target) {
-       n = prob.size();
-       dp.assign(n+1, vector<double>(target+1, 0));
-       dp[n][0] = 1;
+       int n = prob.size();
+       vector<double> prev(target+1, 0), cur(target+1, 0);
+       prev[0] = 1;
        for(int i = n-1; i >= 0; i--){
            for(int j = 0; j <= target; j++){
-                double pick = (j != 0)? dp[i+1][j-1] * prob[i] : 0;
-                double notpick = dp[i+1][j] * (1-prob[i]);
-                dp[i][j] = pick + notpick;               
+                double pick = (j != 0)? prev[j-1] * prob[i] : 0;
+                double notpick = prev[j] * (1-prob[i]);
+                cur[j] = pick + notpick;               
            }
+           prev = cur;
        }
-       return dp[0][target];
+       return cur[target];
     }
 };
