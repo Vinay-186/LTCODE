b75c8c3f38ee5b49dff29219f83504d55ce06715
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-07-26 21:02:45 +0530

    Time: 2 ms (47.79%), Space: 8.3 MB (37.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
         zero_end = 0;
         two_start = n-1;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i <= two_start; i++){
             if(nums[i] < 1 && i > zero_end){
                 swap(nums[i], nums[zero_end++]);
                 i--;
