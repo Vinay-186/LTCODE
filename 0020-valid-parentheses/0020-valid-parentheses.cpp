2e77b72e7e14c77f3ccf19f9bc68c8e28df8cf96
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-08-07 20:23:00 +0530

    Time: 4 ms (34.21%), Space: 6.3 MB (76.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-        map<char, char> mp = {{'[', ']'}, {'{', '}'}, {'(', ')'}};
+        map<char, char> mp = {{')', '('}, {']', '['}, {'}','{'}};
         stack<char> st;
-        for(char& c : s){
-            if(mp.find(c) != mp.end()){
+        for(char c : s){
+            if(st.empty() && mp.find(c) == mp.end() || mp.find(c) == mp.end()){
                 st.push(c);
             }else{
                 if(st.empty()) return false;
-                if(mp[st.top()] != c) return false;
-                else st.pop();
+                if(mp[c] != st.top()) return false;
+                st.pop();
             }
         }
-        return (!st.empty())? false : true;
+        return st.empty();
     }
 };
