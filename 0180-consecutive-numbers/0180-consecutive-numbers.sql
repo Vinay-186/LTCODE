8d33d31246177cab464cdb0dc0581e8f18d0309e
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-08-24 20:16:15 +0530

    Time: 1150 ms (55.16%), Space: 0B (100.00%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select distinct l1.num as ConsecutiveNums from logs l1 inner join logs l2 on l1.id = l2.id -1 and l1.num = l2.num inner join logs l3 on l2.id = l3.id-1 and l2.num = l3.num;
