fabc53eb27e901f7e1115398db05c1da48b3b07a
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-09-17 19:35:25 +0530

    Time: 45 ms (58.93%), Space: 29.7 MB (10.62%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> cache;
-    int endingMask;
-    
-    int dp(int node, int mask, vector<vector<int>>& graph){
-        if(cache[node][mask] != 0) return cache[node][mask];
-        if((mask & (mask-1)) == 0) return 0;
-        cache[node][mask] = INT_MAX-1;
-        for(int neighbour : graph[node]){
-            if((mask & (1 << neighbour)) != 0){
-                int alreadyVisited = dp(neighbour, mask, graph);
-                int notVisited = dp(neighbour, mask ^ (1 << node), graph);
-                int better = min(alreadyVisited, notVisited);
-                cache[node][mask] = min(cache[node][mask] , 1 + better);
-            }
-        }
-        return cache[node][mask];
-    }
-    
     int shortestPathLength(vector<vector<int>>& graph) {
         int n = graph.size();
-        endingMask = (1 << n) -1;
-        cache.assign(n+1, vector<int>(endingMask+1));
-        int best = INT_MAX;
-        for(int node = 0; node < n; node++){
-            best = min(best, dp(node, endingMask, graph));
+        if(n == 1) return 0;
+        int endingMask = (1 << n) -1;
+        vector<vector<int>> seen(n, vector<int>(endingMask, 0));
+        vector<vector<int>> q;
+        for(int i = 0; i <n; i++){
+            q.push_back({i, (1 << i)});
+            seen[i][1 << i] = 1;
         }
-        return best;
+        
+        int steps = 0;
+        while(!q.empty()){
+            vector<vector<int>> next_q;
+            for(int i = 0; i < q.size(); i++){
+                int node = q[i][0];
+                int mask = q[i][1];
+                for(int neighbour : graph[node]){
+                    int nextMask = mask | (1 << neighbour);
+                    if(nextMask == endingMask) return 1+steps;
+                    if(!seen[neighbour][nextMask]){
+                        seen[neighbour][nextMask] =1;
+                        next_q.push_back({neighbour, nextMask});
+                    }
+                }
+            }
+            steps++;
+            q = next_q;
+        }
+        return -1;
     }
 };
