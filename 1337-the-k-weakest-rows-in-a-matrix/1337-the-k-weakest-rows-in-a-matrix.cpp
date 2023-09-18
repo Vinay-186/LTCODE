c0fa336bd93bcef4a9bba4eddb43ebc8d8986765
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-09-18 13:20:33 +0530

    Time: 6 ms (91.97%), Space: 10.6 MB (20.16%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
+    int binarySearch(vector<int>& arr, int n){
+        int low = 0, high = n-1;
+        while(low < high){
+            int mid = (low + high) >> 1;
+            (arr[mid] == 1)? low = mid + 1 : high = mid;
+        }
+        return low;
+    }
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         priority_queue<pair<int,int>> pq;
         int n = mat.size();
+        int col_size = mat[0].size();
         for(int i = 0; i <n ;i++){
             int count = 0;
-            for(int j : mat[i]) if(j) count++;
-            if(pq.size() == k){
-                if(pq.top().first > count){
-                    pq.pop();
-                    pq.push({count, i});
-                }
-            }else{
-                pq.push({count, i});
-            }
+            if(mat[i][col_size-1] == 1) count = col_size;
+            else count = binarySearch(mat[i], col_size);
+            pq.push({count, i});
+            if(pq.size() > k) pq.pop();
         }
         vector<int> ans;
         while(!pq.empty()){
