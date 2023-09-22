93e0f19a2e57163c54cbf77afe3689da9e630ccb
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-09-22 17:44:48 +0530

    Time: 68 ms (27.54%), Space: 44.8 MB (18.84%) - LeetHub

--- 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.cpp
+++ 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     int validSubarrays(vector<int>& nums) {
         int n = nums.size();
-        vector<int> count(n, 1);
-        stack<pair<int,int>> st;
-        for(int i = 0; i < n-1; i++){
-            if(!st.empty() && nums[i] < st.top().first){
-                while(!st.empty() && nums[i] < st.top().first){
-                    int len = i - st.top().second;
-                    count[st.top().second] += len-1;
-                    st.pop();
-                }
+        int count = n;
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && nums[i] < nums[st.top()]){
+                int len = i - st.top();
+                count += len-1;
+                st.pop();
             }
-            if(nums[i] <= nums[i+1]) st.push({nums[i], i});
+            st.push(i);
         }
         while(!st.empty()){
-            if(st.top().first > nums[n-1]){
-                int len = n-1-st.top().second;
-                count[st.top().second] += len-1;
-                st.pop();
-            }else{
-                int len = n - st.top().second;
-                count[st.top().second] += len-1;
-                st.pop();
-            }
+            int len = n - st.top();
+            count += len-1;
+            st.pop();
         }
-        return accumulate(count.begin(), count.end(), 0);
+        return count;
     }
 };
