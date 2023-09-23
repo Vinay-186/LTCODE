fd61e3f56e2c72345964781bac5eb580fff8e379
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-09-23 15:31:53 +0530

    Time: 870 ms (96.80%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,2 @@
 # Write your MySQL query statement below
-SELECT
-customer_id
-FROM customer c 
-GROUP BY customer_id
-HAVING count(DISTINCT(c.product_key)) = (SELECT count(distinct(product_key)) FROM product)
+select customer_id from customer group by customer_id having count(distinct product_key) = (select count(*) from product);
