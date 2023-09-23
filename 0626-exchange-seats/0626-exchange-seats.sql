6055bfaaf9c8778e5596773b6655343a539effd2
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-09-23 20:31:40 +0530

    Time: 763 ms (65.04%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,12 +1,7 @@
-# Write your MySQL query statement below
-select s1.id, s2.student from seat s1 INNER JOIN seat s2
-on s2.id = s1.id + 1 and s1.id % 2 = 1
+select if (
 
-UNION 
-select s2.id, s1.student from seat s1 INNER JOIN seat s2
-on s2.id = s1.id + 1 and s2.id % 2 = 0
+id%2 <>0,
+if( id<> (select max(id) from seat), id+1, id),
+id-1
 
-UNION 
-select id, student from seat where id = (select max(id) from seat) and id % 2 = 1
-
-order by id;
+) id, student from seat order by id;
