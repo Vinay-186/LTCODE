e486ae1559a090e9ba2a3baa2635e58ef616b87c
Vinay-186 <98889767+Vinay-186@users.noreply.github.com>
2023-09-30 19:00:51 +0530

    Time: 23 ms (53.54%), Space: 15.8 MB (64.69%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    vector<int> visited;
-    void dfs(int ind, vector<vector<int>>& adj){
-        visited[ind] = 1;
-        for(int& i : adj[ind]){
-            if(!visited[i]){
-                dfs(i, adj);
-            }
+    vector<int> parent, rank;
+    int count;
+    int findPar(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    
+    void union_ds(int node1, int node2){
+        int par1 = findPar(node1);
+        int par2 = findPar(node2);
+        if(par1 == par2) return;
+        if(rank[par1] < rank[par2]) parent[par1]= par2;
+        else if(rank[par2]< rank[par1]) parent[par2] = par1;
+        else{
+            parent[par1] = par2;
+            rank[par2]++;
         }
     }
-    int findCircleNum(vector<vector<int>>& c) {
-        int n = c.size();
-        visited.assign(n,0);
-        vector<vector<int>> adj(n);
-        for(int i = 0; i < n; i++){
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        count = n;
+        rank.resize(n, 1);
+        parent.resize(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
+        for(int i = 0; i <n;i++){
             for(int j = 0; j < n; j++){
-                if(c[i][j]){
-                    adj[i].push_back(j);
-                    if(i != j) adj[j].push_back(i);
+                if(i == j) continue;
+                if(isConnected[i][j] && findPar(i) != findPar(j)){
+                    count--;
+                    union_ds(i, j);
                 }
-            }
-        }
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                count++;
-                dfs(i, adj);
             }
         }
         return count;
